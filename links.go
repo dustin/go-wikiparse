40e7cf7c7af2764a9681a83b4cbfe1d61d5ec9d0
Dustin Sallings <dustin@spy.net>
2012-10-11 08:57:59 -0700

    Added link parsing.

---
+++ links.go
@@ -0,0 +1,24 @@
+package wikiparse
+
+import (
+	"regexp"
+)
+
+var linkRE *regexp.Regexp
+
+func init() {
+	linkRE = regexp.MustCompile(`\[\[([^\|\]]+)`)
+}
+
+// Find all the links from within an article body.
+func FindLinks(text string) []string {
+	cleaned := nowikiRE.ReplaceAllString(commentRE.ReplaceAllString(text, ""), "")
+	matches := linkRE.FindAllStringSubmatch(cleaned, 100000)
+
+	rv := []string{}
+	for _, x := range matches {
+		rv = append(rv, x[1])
+	}
+
+	return rv
+}
